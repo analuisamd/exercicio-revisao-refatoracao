2c87816f6f89ff74a9c96e73ffb687110a210919
ana luisa <analuisamends@gmail.com>
2020-10-20 11:25:36 -0300

    alterei o arq Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,18 @@
+  
 #include <string>
 #include "Empregado.hpp"
 
 class Engenheiro : public Empregado {
 
   public:
-    std::string nome;  
 	int projetos;
+
+
+  void printEngenheiro(double horas){
+		std::cout << "Nome: " <<  nome <<std::endl;
+		std::cout << "Salario Mes: " << Empregado::pagamentoMes(horas) << std::endl;
+		std::cout << "Projetos: " << projetos << std::endl; 
+    std::cout << std::endl;
+	};
 	
 };
-
