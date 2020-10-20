33f31c7ee9b47ae764696175bd3a1e4de536ec96
ana luisa <analuisamends@gmail.com>
2020-10-20 11:23:42 -0300

    alterei o arq Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,22 @@
 #include <string>
 #include "Empregado.hpp"
 
+#define QUANT_MESES 12
 class Vendedor : public Empregado {
 
   public:
-    std::string nome;  	  
+
+  double quotaMensalVendas; 	  
 	
 	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
+	  return quotaMensalVendas * QUANT_MESES;
 	}
-	
-};
 
+	void printVendedor(double horas){
+		std::cout << "Nome: " << nome <<std::endl;
+		std::cout << "Salario Mes: " << Empregado::pagamentoMes(horas) << std::endl;
+		std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl; 
+		std::cout << std::endl;
+	};
+
+};
